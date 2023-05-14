1cd570faf29d2723cfa91c091451cdba5cab31bd
Lucas Rocha <lucasregodarocha@outlook.com>
2023-05-14 14:57:38 -0300

    fix: fixed the enum BellongMonth and interface Day export

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {createMonthStructure} from './Components/createMonthStructure';
 export {createMonthStructureFlaggingDaysAccordingMonth} from './Components/createMonthStructureFlaggingDaysAccordingMonth';
+export {Day} from './Components/utils';
+export {BellongMonth} from './Components/utils';
